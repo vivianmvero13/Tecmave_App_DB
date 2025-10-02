4f77dd539a57b705f30e1945eb4ee70ebaf6b184
Vivian Velazquez <vivianmvero@gmail.com>
2025-10-01 23:45:00 -0600

    Correcciones menores

---
+++ Tecmave/Tecmave.Api/Models/RoleChangeAudit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tecmave.Api.Models
+{
+    public class RoleChangeAudit
+    {
+        public long Id { get; set; }
+
+        public int TargetUserId { get; set; }
+        public string? TargetUserName { get; set; }
+
+        public string? PreviousRole { get; set; }
+        public string? NewRole { get; set; }
+
+        public int? ChangedByUserId { get; set; }
+        public string? ChangedByUserName { get; set; }
+
+        public DateTime ChangedAtUtc { get; set; } = DateTime.UtcNow;
+        public string Action { get; set; } = "assign";
+        public string? Detail { get; set; }
+        public string? SourceIp { get; set; }
+    }
+}
--- Tecmave/Tecmave.Api/Program.cs
+++ Tecmave/Tecmave.Api/Program.cs
@@ -75,5 +75,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
