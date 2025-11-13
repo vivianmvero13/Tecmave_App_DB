a832aca2c4504b9262ae95f3e88bb94acc173975
Kaled <dgonzalez00893@ufide.ac.cr>
2025-11-13 17:35:54 -0600

    arreglando colaboradores y planillas

--- Tecmave/Front/Pages/Colaboradores/Index.cshtml.cs
+++ Tecmave/Front/Pages/Colaboradores/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Front.Data;
-using Front.Models;
+
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,18 +9,10 @@ namespace Front.Pages.Colaboradores
 {
     public class IndexModel : PageModel
     {
-        private readonly MyIdentityDBContext _context;
 
-        public IndexModel(MyIdentityDBContext context)
+
+        public void OnGet()
         {
-            _context = context;
-        }
-
-        public IList<Colaborador> Colaboradores { get; set; } = new List<Colaborador>();
-
-        public async Task OnGetAsync()
-        {
-            Colaboradores = await _context.Set<Colaborador>().ToListAsync();
         }
     }
 }
