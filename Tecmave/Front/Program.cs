314584903ccad3707bc64e83c88d27d534cc28b2
Kaled <dgonzalez00893@ufide.ac.cr>
2025-12-04 12:33:41 -0600

    Contacto funcionando, ubicacion funcionando e imagen arreglada

--- Tecmave/Front/Program.cs
+++ Tecmave/Front/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 // ======================
 builder.Services.AddRazorPages();
 
+
+
 // ======================
 //     HTTP CLIENT API
 // ======================
