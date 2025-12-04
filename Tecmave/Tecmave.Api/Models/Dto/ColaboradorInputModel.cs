3cb4f9e5abbe6fec4b246aff49d912ec51c4374a
Kaled <dgonzalez00893@ufide.ac.cr>
2025-12-04 14:48:35 -0600

    Eliminamos migraciones y modificamos colabordores

---
+++ Tecmave/Tecmave.Api/Models/Dto/ColaboradorInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Tecmave.Api.Models.Dto
+{
+    public class ColaboradorInputModel
+    {
+        // Datos del usuario
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Rol { get; set; }
+
+        // Datos del colaborador
+        public string Puesto { get; set; }
+        public decimal Salario { get; set; }
+
+        // El cliente puede enviarla o se puede asignar automáticamente
+        public DateOnly? FechaContratacion { get; set; }
+    }
+}
